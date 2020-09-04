ac4bd1f31011f5a0e130ce2d245ee135b205b1da
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2020-09-03 20:53:42 -0400

    time works

--- arduino/0time.ino
+++ arduino/0time.ino
@@ -8,52 +8,59 @@ RtcDS3231<myWire> Rtc(I2C);
 
 class timer
 {
-  private:
-    time_t now = (time_t) MIN_TIME;
+private:
+  time_t now = (time_t)MIN_TIME;
+  bool valid = true;
 
-    bool valid = true;
+public:
+  bool timeSet = false;
+  void init();
+  unsigned long getTime();
+  void setTime(unsigned long t);
+  bool isValid();
 
-  public:
-    bool timeSet = false;
-
-    unsigned long getTime();
-    void setTime(unsigned long t);
-    bool isValid();
-    timer();
+  timer();
 };
 
 timer::timer()
 {
-  Serial.println("t");
   Rtc.Begin();
-  Serial.println("t");
+}
 
+void timer::init()
+{
+  Serial.println("1");
   if (!Rtc.GetIsRunning())
   {
     //if rtc not running
     Rtc.SetIsRunning(true);
-  } Serial.println("t");
+  }
 
-
-  now = (time_t)Rtc.GetTime(); Serial.println("tim");
-
+  now = (time_t)Rtc.GetTime();
+  Serial.println(now);
+  delay(300);
   if ((!isValid()) & (now < MIN_TIME))
   {
     now = (time_t)MIN_TIME;
     Rtc.SetTime(&now);
     valid = false;
   }
-  Serial.println("tim");
+  delay(300);
+  now = (time_t)Rtc.GetTime();
+  Serial.println(now);
+  delay(300);
+  Serial.println("1");
 
   Rtc.Enable32kHzPin(false);
   Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
+  Serial.println("1");
 }
 unsigned long timer::getTime()
 {
-  if (isValid())
+  if (!isValid())
   {
-    return 0;
     Serial.println(F("getfailed"));
+    return 0;
   }
   now = Rtc.GetTime();
   return (unsigned long)now;
@@ -66,6 +73,25 @@ void timer::setTime(unsigned long t)
 }
 bool timer::isValid()
 {
-  valid &= Rtc.IsDateTimeValid();
-  return valid;
+  return valid & Rtc.IsDateTimeValid();
+}
+timer myTimer;
+
+void setup()
+{
+  Serial.begin(SERIAL_SPEED);
+
+  myTimer.init();
+  myTimer.setTime(1599179228);
+  
+  Serial.println("setup");
+}
+
+void loop()
+{
+  time_t now = myTimer.getTime();
+
+  Serial.println(now);
+
+  delay(1000);
 }
